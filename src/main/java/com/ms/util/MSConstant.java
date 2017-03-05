91d620a3d812075cec4abe56fe4d9920fb4255f6
Amit Agnihotri <amit.agnihotri@akosha.com>
2017-03-05 08:58:19 +0530

    Base util class added

---
+++ src/main/java/com/ms/util/MSConstant.java
@@ -0,0 +1,17 @@
+package com.ms.util;
+
+/**
+ * @author amit
+ *
+ */
+public interface MSConstant {
+	 String EMAIL_NAME_SEPARATOR ="###" ;
+	 String EMAIL_PATTERN = "(.+)@(.+)";
+	 String SUCCESS = "success";
+	 String ERROR = "error";
+	 String MESSAGE = "message";
+	 String SUCCESS_MESSAGE = "Successfully submitted";
+	 String ERROR_MESSAGE = "Error Occured";
+	 String REQ_MESSAGE = "reqmsg";
+	 String SALT = "MS#2";
+}
